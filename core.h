e9b8700941a4a0c36aa1425286d2870ac5d07af6
luis daniel <luisdaniel.ta@outlook.com>
2023-05-18 22:27:29 -0500

    mode_usb_debug

---
+++ core.h
@@ -0,0 +1,11 @@
+#ifndef CORE_H
+#define CORE_H
+#include "pico/stdlib.h"
+#include "pico/bootrom.h"
+#include "pico/multicore.h"
+
+void reload_program();
+void mode_usb_debug(bool);
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-#include "pico/stdlib.h"
-#include "hardware/pwm.h"
-#include "pico/bootrom.h"
 #include <stdint.h>
+#include "pico/stdlib.h"
+#include "core.h"
 #include "motor.h"
 #include "control_motor.h"
-
-void reload_program();
-bool reload_pin_previous = true;
-#define RELOAD_PIN 16
 
 uint8_t pins_left[2] = {14, 13};
 uint8_t pins_right[2] = {12, 11};
@@ -21,28 +16,11 @@ control_motor motors(motor_left, motor_right, en);
 int main()
 {
     stdio_init_all();
-
-    gpio_init(RELOAD_PIN);
-    gpio_set_dir(RELOAD_PIN, GPIO_IN);
-    gpio_pull_up(RELOAD_PIN);
+    mode_usb_debug(true);
 
     while (true)
     {
-        reload_program();
         motors.forward();
     }
     return 0;
-}
-
-void reload_program()
-{
-    /*
-        The reload_program function is designed to check
-        the state of a specific pin (RELOAD_PIN) and initiate
-        a USB boot mode if a certain condition is met.
-    */
-    bool reload_pin_current = !gpio_get(RELOAD_PIN);
-    if (reload_pin_current && !reload_pin_previous)
-        reset_usb_boot(0, 0);
-    reload_pin_previous = reload_pin_current;
 }
